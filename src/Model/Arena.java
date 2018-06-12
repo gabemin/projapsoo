7b22ffdf9112e67d9a447f315cae4953d6bc4b63
gabemin <g.sminetto94@gmail.com>
2018-06-12 10:20:08 -0400

    implementei metodos nas classes character e arena(antigo background)

---
+++ src/Model/Arena.java
@@ -0,0 +1,70 @@
+package Model;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class Arena extends JPanel implements ActionListener {
+
+        private Timer timer;
+        private Character character;
+        private final int DELAY=10;
+        private String caminho;
+
+    public Arena() {
+        initArena();
+    }
+    //inicializando o mapa do jogo
+    private void initArena() {
+        addKeyListener(new TAdapter());
+        setFocusable(true);
+        setBackground(Color.black);
+        setDoubleBuffered(true);
+
+        caminho = "#";
+        character = new Character(caminho);
+
+        timer = new Timer(DELAY, this);
+        timer.start();
+    }
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        doDrawing(g);
+
+        Toolkit.getDefaultToolkit().sync();
+    }
+
+    private void doDrawing(Graphics g) {
+
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawImage(character.getSprite(), character.getX(),
+                character.getY(), this);
+
+    }
+    //chamado a cada DELAY ms. chama o metodo step()
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        step();
+    }
+    //redesenha o personagem na nova localização
+    private void step() {
+        character.move();
+        repaint(character.getX()-1, character.getY()-1,
+                character.getSpriteWidth()+2, character.getSpriteHeight()+2);
+    }
+    private class TAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            character.keyPressed(e);
+        }
+        @Override
+        public void keyReleased(KeyEvent e) {
+            character.keyReleased(e);
+        }
+    }
+}
