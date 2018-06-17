43a5f2bf96fc0b4b4eac39dcb84f7ab8dee9b0d0
gabemin <g.sminetto94@gmail.com>
2018-06-17 12:26:15 -0400

    comit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+//import View.ArenaComponent;
+
+import Controller.ArenaController;
+import Controller.CharacterController;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Timer;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+        ArenaController arenaControl = new ArenaController();
+    }
+}
+
